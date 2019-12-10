630d3c271dc56ac248520aba8d88164b95af40ab
Michael Schiumo <mschiumo18@gmail.com>
2019-12-09 22:41:13 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,32 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+import Keypad from './Keypad.js';
+
+export default class EyesOnMe extends Component {
+
+
+  focus = () => {
+    console.log("Good!")
+  }
+
+  blur = () => {
+    console.log("Hey! Eyes on me!")
+  }
+
+  render() {
+    return (
+      <button>
+      onFocus={this.focus}
+      onBlur={this.blur}
+      </button>
+    )
+  }
+
+}
+
+//
+// tickle = () => {
+//    console.log('Tee hee!');
+//  }
+//
+//  <button onClick={this.tickle}>Tickle me!</button>
